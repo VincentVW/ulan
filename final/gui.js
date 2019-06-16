d1dbc2fb52c1ff2cd6d5cfac24274931101430c8
Vincent van 't Westende <vincent.vantwestende@hatchstudio.co>
2019-06-16 17:39:58 +0200

    small changes?

--- final/gui.js
+++ final/gui.js
@@ -1,23 +1,29 @@
 var gui;
+var guiVisible = false;
+
+var bController, cController, gController, dController, alphaController, dnaSpeedController;
 
 function initGui() {
   // Create Layout GUI
   gui = new dat.gui.GUI({ name: "UB GUI" });
   gui.useLocalStorage = true;
 
-  var bController = gui.add(variables, "bucketSize", 25, 100, 1);
-  var cController = gui.add(variables, "c", 1500000, 2500000, 10000);
-  var gController = gui.add(variables, "gap", 13, 50, 1);
-  var dController = gui.add(variables, "drag", 0.5, 1.2, 0.01);
-  gui.add(variables, "minAlpha", 80, 255, 1);
-  gui.add(variables, "speed", 0, 1.0, 0.01);
-  gui.add(variables, "dnaSpeed", 0, 1.0, 0.01);
+  bController = gui.add(variables, "bucketSize", 25, 100, 1);
+  cController = gui.add(variables, "c", 1500000, 2500000, 10000);
+  gController = gui.add(variables, "gap", 13, 50, 1);
+  dController = gui.add(variables, "drag", 0.5, 1.2, 0.01);
+  
+  alphaController = gui.add(variables, "minAlpha", 80, 255, 1);
+  speedController = gui.add(variables, "speed", 0, 1.0, 0.01);
+  dnaSpeedController = gui.add(variables, "dnaSpeed", 0, 1.0, 0.01);
   gui.remember(variables);
 
   bController.onFinishChange(reInit);
   cController.onFinishChange(reInit);
   gController.onFinishChange(reInit);
   dController.onFinishChange(reInit);
+
+  gui.hide();
 }
 
 var reInit = function(value) {
@@ -30,3 +36,14 @@ var reInit = function(value) {
   population = new Population();
   population.init();
 };
+
+// check for keyboard events
+function keyPressed() {
+  switch(key) {
+    // type [F1] to hide / show the GUI
+    case 'p':
+        guiVisible = !guiVisible;
+      if(guiVisible) gui.show(); else gui.hide();
+      break;
+  }
+}
--- final/osc.js
+++ final/osc.js
@@ -1,4 +1,10 @@
 // CONFIGURATION
+
+// if enabled, the node server should be running, this can be done through:
+// `npm run osc-server`
+// and it requires node and npm to be installed, and `npm install` to have been run once from the final folder.
+let useOsc = false;
+
 let oscInputPort = 7400;
 let oscOutputPort = 3334;
 
@@ -11,22 +17,27 @@ function initOsc() {
 }
 
 function setupOsc(oscPortIn, oscPortOut) {
+  if (!useOsc) {
+    return;
+  }
   var socket = io.connect("http://127.0.0.1:8081", {
     port: 8081,
     rememberTransport: false
   });
   socket.on("connect", function() {
     socket.emit("config", {
-      server: { port: oscPortIn, host: "192.168.178.101" },
+      server: { port: oscPortIn, host: "192.168.0.2" },
       client: { port: oscPortOut, host: "127.0.0.1" }
     });
   });
   socket.on("message", function(msg) {
+    console.log(msg)
     receiveOsc(msg);
   });
 }
 
 function receiveOsc(msg) {
+  
   if (msg.length > 2) {
     oscInputX = ((msg[1] + 1) / 2) * 1920;
     oscInputY = Math.abs((msg[2] - 1) / 2) * 1080;
